2df21d36cc7d0a13e34aca74005ae2af205b8ae0
Martin-Niemann <martinmail72@gmail.com>
2021-12-02 13:06:09 +0100

    Hello

--- FisketorvetApp/Startup.cs
+++ FisketorvetApp/Startup.cs
@@ -30,6 +30,7 @@ namespace FisketorvetApp
             services.AddTransient<IStoreRepository, AStoreJsonFile>();
             services.AddSession();
             services.AddMemoryCache();
+            // Hello
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
