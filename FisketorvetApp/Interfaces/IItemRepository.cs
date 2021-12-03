eb477bcb96deb4ba7a3c20d725d1ca791de1a1b1
Martin-Niemann <martinmail72@gmail.com>
2021-12-03 10:44:58 +0100

    Add item model (abstract class, clothes and its inheriting classes), repository interface, repository and jsonhelpers.

---
+++ FisketorvetApp/Interfaces/IItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using FisketorvetApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// Martin
+
+namespace FisketorvetApp.Interfaces
+{
+    public interface IItemRepository
+    {
+        public List<AItem> AllItems();
+        
+        public List<AItem> Filter_By_StartCriteria(string criteria);
+
+        public AItem GetItem(int id);
+    }
+}
---
+++ FisketorvetApp/Repositories/AItemJsonFile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using FisketorvetApp.Helpers;
+using FisketorvetApp.Interfaces;
+using FisketorvetApp.Models;
+
+// Martin
+
+namespace FisketorvetApp.Repositories
+{
+    public class AItemJsonFile : IItemRepository
+    {
+        string JsonFilePath = @"C:\Users\Lenovo\OneDrive - Zealand\Software design\Project work\1 Semester exams project\FisketorvetAppPart2\FisketorvetApp\Data\JsonStores.json";
+        private List<AItem> items;
+
+        public AItemJsonFile()
+        {
+            items = JsonHelper.ReadItemFile(JsonFilePath);
+        }
+        
+        public List<AItem> AllItems()
+        {
+            return JsonHelper.ReadItemFile(JsonFilePath);
+        } 
+
+        public List<AItem> Filter_By_StartCriteria(string criteria)
+        {
+            List<AItem> filteredItems = new List<AItem>();
+
+            foreach (AItem item in items)
+            {
+                if (item.Name.StartsWith(criteria))
+                {
+                    filteredItems.Add(item);
+                }
+            }
+
+            return filteredItems;
+        }
+
+        public AItem GetItem(int id)
+        {
+            foreach (AItem item in items)
+            {
+                if (item.Id == id)
+                {
+                    return item;
+                }
+            }
+            
+            return null;
+        }
+    }
+}
